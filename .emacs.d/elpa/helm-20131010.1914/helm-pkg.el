11de2d3d4a55579b4a989d709fe4940cfdc3cf0e
hironemu <hironemu@gmail.com>
2013-10-11 12:11:43 +0900

    パッケージ更新

---
+++ .emacs.d/elpa/helm-20131010.1914/helm-pkg.el
@@ -0,0 +1,2 @@
+(define-package "helm" "20131010.1914" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
+
---
+++ .emacs.d/elpa/ruby-end-20131010.1958/ruby-end-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ruby-end" "20131010.1958" "Automatic insertion of end blocks for Ruby [github]" (quote nil))
